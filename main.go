c0783fb82fd4e0333a3ee1328914eab7a57f27a9
e-ukiyo <87203515+hacywhatt@users.noreply.github.com>
2026-04-24 21:55:50 +0300

    Update main.go

--- main.go
+++ main.go
@@ -15,9 +15,54 @@ type DorkResult struct {
 
 func generateDorks(keyword string) []DorkResult {
 	templates := map[string][]string{
-		"Sqli":   {"inurl:index.php?id=", "intext:\"sql syntax near\""},
-		"Önemli": {"extension:config", "extension:bak"},
-		"Osint":  {"site:linkedin.com \"+90\"", "site:github.com \"@gmail.com\""},
+		"Sqli":   "inurl:index.php?id=", 
+			"intext:\"sql syntax near\"",
+			"intext:SQL syntax & inurl:index.php?=id",
+		"Önemli": "extension:config", 
+			"extension:bak",
+			"site:github.com \"BEGIN OPENSSH PRIVATE KEY\"",
+			"site:*.*.* intitle:\"index of\" *.pcapng",
+			"intitle:\"index of\" \"configuration.php\"",
+			"site:com inurl:invoice",
+			"intitle:index.of /logs.txt",
+			"inurl:\"/wp-content/debug.log\"",
+			"intitle:\"index of\" \"config.php.txt\"",
+			"intitle:\"index of\" \"*robots.txt\" site:.edu",
+			"filetype:log",
+			"intext:\"index of\" \"phpinfo\" site:*.in",
+			"inurl:/default.rdp",
+		"Osint": "site:linkedin.com \"+90\"", 
+			"site:github.com \"@gmail.com\"",
+			"site:linkedin.com \"@gmail.com\"",
+			"site:linkedin.com \"Phone:\" \"+90\"",
+			"site:linkedin.com \"Cyber Security Analyst\" \"Phone * * *\"",
+			"site:.edu intext:\"robotics\" inurl:/research",
+		"Admin": {
+			"site:login.*.* site:portal.*.*",
+			"site:uat.* * inurl:login",
+			"intext:\"user\" filetype:php intext:\"account\" inurl:/admin",
+			"inurl:adminpanel site:*.* -site:github.com",
+			"site:login.*.* | site:portal.*.*",
+			"inurl: edu + site: admin",
+			"inurl:/admin.php",
+			"intitle:\"index of\" intext: \"login.php\"",
+			"site:.co.in intitle:index of /wp-admin",
+			"intitle:index.of login.js",
+			"inurl:\"admin/default.aspx\"",
+			"inurl:\"UserLogin/\" intitle:\"Panel\"",
+		},
+		"Webcam": {
+			"intitle:\"Webcam\" inurl:WebCam.htm",
+			"inurl:home.htm intitle:1766",
+			"intitle:\"webcam\" \"login\"",
+		},
+
+		"Db": {
+			"intitle:\"Index of /databases\"",
+			"intitle:index.of./.database",
+			"intitle:\"index of\" mysql inurl:./db/",
+			"\"structure\" + ext:sql",
+		},
 	}
 
 	var results []DorkResult
